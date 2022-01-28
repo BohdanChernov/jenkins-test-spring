585c98efc68074b75787facc1da936268da590de
Bohdan <b.chernov.kiev@gmail.com>
2022-01-28 14:28:43 +0200

    Initial commit

---
+++ src/test/java/com/example/jenkinstest/JenkinsTestApplicationTests.java
@@ -0,0 +1,18 @@
+package com.example.jenkinstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class JenkinsTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+        String expected = "Baeldung";
+        String actual = "Baeldung";
+        assertEquals(expected, actual);
+    }
+
+}
